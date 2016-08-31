de6a3c8ccf9421d9f8907f0909b3a1050a5e212e
Roman Eckert <mail@romaneckert.de>
2016-08-31 23:00:52 +0200

    add bootstrap & jquery

--- app/controllers/RobotController.php
+++ app/controllers/RobotController.php
@@ -4,6 +4,15 @@ use Phalcon\Mvc\Controller;
 
 class RobotController extends Controller {
 
+    public function initialize() {
+        $this->assets->addCss('css/bootstrap.min.css');
+        $this->assets->addCss('css/bootstrap-theme.min.css');
+        $this->assets->addCss('css/style.css');
+
+        $this->assets->addJs('js/jquery.min.js');
+        $this->assets->addJs('js/bootstrap.min.js');
+    }
+
     public function dashboardAction() {
 
     }
