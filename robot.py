4f7216aaf1c47ea78edf32b83e3d9edff32332a9
Roman Eckert <mail@romaneckert.de>
2016-08-31 20:44:22 +0200

    optimize sound mary tts test

--- robot.py
+++ robot.py
@@ -3,34 +3,35 @@ from bionics.controller import Controller
 from bionics.logger import Logger
 import RPi.GPIO as GPIO
 import urllib.request
+import urllib.parse
 import os
 
 class Robot(Controller):
 
-    motor = None
-    cycle1 = 6
-    cycle2 = 6
-    cycle3 = 6
-    cycle_direction1 = 1
-    cycle_direction2 = 1
-    cycle_direction3 = 1
+    #motor = None
+    #cycle1 = 6
+    #cycle2 = 6
+    #cycle3 = 6
+    #cycle_direction1 = 1
+    #cycle_direction2 = 1
+    #cycle_direction3 = 1
 
     def __init__(self):
 
         Controller.__init__(self)
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(23, GPIO.OUT)
-        GPIO.setup(17, GPIO.OUT)
-        GPIO.setup(25, GPIO.OUT)
-        self.motor1 = GPIO.PWM(23, 50)
-        self.motor2 = GPIO.PWM(17, 50)
-        self.motor3 = GPIO.PWM(25, 50)
-        self.motor1.start(self.cycle1)
-        self.motor2.start(self.cycle2)
-        self.motor3.start(self.cycle3)
+        #GPIO.setmode(GPIO.BCM)
+        #GPIO.setup(23, GPIO.OUT)
+        #GPIO.setup(17, GPIO.OUT)
+        #GPIO.setup(25, GPIO.OUT)
+        #self.motor1 = GPIO.PWM(23, 50)
+        #self.motor2 = GPIO.PWM(17, 50)
+        #self.motor3 = GPIO.PWM(25, 50)
+        #self.motor1.start(self.cycle1)
+        #self.motor2.start(self.cycle2)
+        #self.motor3.start(self.cycle3)
 
-        #self.playMessage('Hello')
+        self.playMessage('Hello')
         #Logger.log('hello', 'info')
         #self.activate()
         GPIO.setwarnings(False)
@@ -39,11 +40,23 @@ class Robot(Controller):
 
 
     def playMessage(self, message):
-        urllib.request.urlretrieve('http://mary.dfki.de:59125/process?INPUT_TEXT=Starte%20Systeme!&INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&AUDIO=WAVE_FILE&LOCALE=de', 'wav.wav')
+
+        params = (('INPUT_TEXT', 'Alle Systeme aktiv'),
+                  ('INPUT_TYPE', 'TEXT'),
+                  ('OUTPUT_TYPE', 'AUDIO'),
+                  ('AUDIO', 'WAVE_FILE'),
+                  ('LOCALE', 'de'),
+                  ('effect_Volume_selected', 'on'),
+                  ('effect_Volume_parameters', 'amount:2.0'))
+                  #('effect_Chorus_selected', 'on'),
+                  #('effect_Chorus_parameters', 'delay1:466;amp1:0.54;delay2:600;amp2:-0.10;delay3:250;amp3:0.30'))
+
+        urllib.request.urlretrieve('http://mary.dfki.de:59125/process?' + urllib.parse.urlencode(params), 'wav.wav')
         os.system('mplayer wav.wav')
 
     def update(self):
 
+        """
         self.cycle1 += self.delta_time * self.cycle_direction1 * 5
 
         if self.cycle1 < 6:
@@ -79,6 +92,8 @@ class Robot(Controller):
         self.motor3.ChangeDutyCycle(self.cycle3)
 
         print(str(self.delta_time) + " " + str(self.cycle1))
+
+        """
 
 robot = Robot()
 
