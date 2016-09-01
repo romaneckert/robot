f043bdf08da4f6d8b156c1c7e576460aedaf5b64
Roman Eckert <mail@romaneckert.de>
2016-09-01 21:47:45 +0200

    add compass config

--- app/controllers/RobotController.php
+++ app/controllers/RobotController.php
@@ -7,7 +7,7 @@ class RobotController extends Controller {
     public function initialize() {
         $this->assets->addCss('css/bootstrap.min.css');
         $this->assets->addCss('css/bootstrap-theme.min.css');
-        $this->assets->addCss('css/style.css');
+        $this->assets->addCss('css/style.min.css');
 
         $this->assets->addJs('js/jquery.min.js');
         $this->assets->addJs('js/bootstrap.min.js');
---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path = "public"
+css_dir = "public/css"
+sass_dir = "public/sass"
+images_dir = "public/img"
+
+cache_path = '.sass-cache'
+
+sourcemap = true
