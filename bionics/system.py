cb6059a98b92bcff1a3ea81d48bc81dc0b91916e
Roman Eckert <mail@romaneckert.de>
2016-09-09 20:38:42 +0200

    activate chorus

--- bionics/system.py
+++ bionics/system.py
@@ -25,9 +25,9 @@ class System:
                   ('AUDIO', 'WAVE_FILE'),
                   ('LOCALE', 'de'),
                   ('effect_Volume_selected', 'on'),
-                  ('effect_Volume_parameters', 'amount:2.0'))
-                  #('effect_Chorus_selected', 'on'),
-                  #('effect_Chorus_parameters', 'delay1:466;amp1:0.54;delay2:600;amp2:-0.10;delay3:250;amp3:0.30'))
+                  ('effect_Volume_parameters', 'amount:2.0'),
+                  ('effect_Chorus_selected', 'on'),
+                  ('effect_Chorus_parameters', 'delay1:466;amp1:0.54;delay2:600;amp2:-0.10;delay3:250;amp3:0.30'))
 
         urllib.request.urlretrieve('http://mary.dfki.de:59125/process?' + urllib.parse.urlencode(params), 'wav.wav')
         os.system('mplayer wav.wav')
