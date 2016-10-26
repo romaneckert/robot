65f6126084bb001e98d0d78f99a0e65594e29f86
Roman Eckert <roman.eckert@inblau.de>
2016-10-26 14:58:22 +0200

    optimize api controller system information

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -4,17 +4,27 @@ use Phalcon\Mvc\Controller;
 
 class ApiController extends Controller {
 
+    /**
+     * @return \Phalcon\Http\Response|\Phalcon\Http\ResponseInterface
+     */
     public function dataAction() {
+
+        // disable view in json api
         $this->view->disable();
 
+        // get system information
         $data['system'] = [];
-        $data['system']['load'] = EnvUtility::getLoad();
+        $data['system']['load'] = EnvUtility::getLoadAverage();
         $data['system']['processes'] = EnvUtility::countProcesses();
         $data['system']['uptime'] = EnvUtility::getUpTime();
 
+        // get global config
         $data['config'] = $this->config;
 
+        // set content type to json
         $this->response->setContentType('application/json', 'UTF-8');
+
+        // encode json data
         $this->response->setContent(json_encode($data));
 
         return $this->response;
--- app/utilities/EnvUtility.php
+++ app/utilities/EnvUtility.php
@@ -1,15 +1,27 @@
 <?php
 
+/**
+ * Class EnvUtility
+ */
 class EnvUtility {
 
-    public static function getLoad() {
+    /**
+     * @return float
+     */
+    public static function getLoadAverage() {
         return round(sys_getloadavg()[0],2);
     }
 
+    /**
+     * @return int
+     */
     public static function countProcesses() {
         return (integer)trim(exec('ps -A | wc -l'));
     }
 
+    /**
+     * @return string
+     */
     public static function getUpTime() {
         return exec("uptime | grep -ohe 'up .*' | sed 's/,//g' | awk '{ print $2}'");
     }
