ca78dac89a739b2dc5c01954558ad06babff91cb
Roman Eckert <mail@romaneckert.de>
2016-11-23 22:20:44 +0100

    optimize camera - test rotation code

--- src/js/interface.js
+++ src/js/interface.js
@@ -5,6 +5,9 @@ Interface.Main = {
     config : null,
     timer : null,
 
+    width: null,
+    height: null,
+
     debug: false,
 
     scene : null,
@@ -16,21 +19,23 @@ Interface.Main = {
     clock : new THREE.Clock(),
 
     init : function() {
-
-        this.config = new Interface.Config();
-
-        console.log(this.config);
-
         $(window).on('load',this.handleWindowLoad.bind(this));
+        $(window).on('resize',this.handleWindowResize.bind(this));
     },
 
     handleWindowLoad : function() {
 
-        this.createScene();
+        this.config = new Interface.Config();
+
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+
+        this.updateScene();
 
         $('body').on('click', 'a', this.handleButtonClick.bind(this));
 
         // create left eye
+        /*
         this.objects.leftEye = new THREE.Object3D();
         this.objects.leftEye.add(new Interface.Line([[0.5,0,0],[1.5,0,0]],this.config.cMain,1,0.5));
         this.objects.leftEye.add(new Interface.Line([[-0.5,0,0],[-1.5,0,0]],this.config.cMain,1,0.5));
@@ -41,39 +46,51 @@ Interface.Main = {
         this.objects.leftEye.position.x = -2;
         this.objects.leftEye.position.y = 1;
         this.scene.add(this.objects.leftEye);
+        */
 
         // create right eye
+        /*
         this.objects.rightEye = new THREE.Object3D();
         this.objects.rightEye.add(new Interface.Circle(1.3,128,[0,0,0],this.config.cMain,1,0.7));
         this.objects.rightEye.add(new Interface.Circle(1.2,128,[0,0,0],this.config.cMain,6,0.05));
         this.objects.rightEye.position.x = 2;
         this.objects.rightEye.position.y = 1;
         this.scene.add(this.objects.rightEye);
+        */
 
-        this.scene.add(new Interface.Line([[-7,2,0],[-4,0,0],[-4,-2,0],[-4.5,-2.5,0]], this.config.cMain,1,0.5));
-        this.scene.add(new Interface.Line([[7,2,0],[4,0,0],[4,-2,0],[4.5,-2.5,0]], this.config.cMain,1,0.5));
+        //this.scene.add(new Interface.Line([[0,0,0],[100,0,0],[100,100,0]], this.config.cMain,1,0.5));
 
-        this.scene.add(new Interface.Line([[-6,1.2,0],[-4.1,-0.05,0],[-4.1,-1,0]], this.config.cMain,3,0.6));
-        this.scene.add(new Interface.Line([[6,1.2,0],[4.1,-0.05,0],[4.1,-1,0]], this.config.cMain,3,0.6));
+        this.scene.add(new Interface.Circle(100,256,[0,160,0],this.config.cMain,4,0.7));
+        this.scene.add(new Interface.Circle(60,256,[0,160,-100],this.config.cMain,2,0.4));
+        this.scene.add(new Interface.Circle(60,256,[0,160,100],this.config.cMain,2,0.4));
 
-        this.scene.add(new Interface.Circle(0.1,128,[0,1,0],this.config.cMain,1,0.7));
-        this.scene.add(new Interface.Circle(0.2,128,[0,1,0],this.config.cMain,1,0.2));
+        this.scene.add(new Interface.Circle(100,256,[0,-160,0],this.config.cMain,4,0.7));
+        this.scene.add(new Interface.Circle(60,256,[0,-160,-100],this.config.cMain,2,0.4));
+        this.scene.add(new Interface.Circle(60,256,[0,-160,100],this.config.cMain,2,0.4));
 
-        this.scene.add(new Interface.Circle(384,128,[0,0,0],this.config.cMain,1,0.7));
+        this.scene.add(new Interface.Circle(100,256,[-220,0,0],this.config.cMain,4,0.7));
+        this.scene.add(new Interface.Circle(60,256,[-220,0,-100],this.config.cMain,2,0.4));
+        this.scene.add(new Interface.Circle(60,256,[-220,0,100],this.config.cMain,2,0.4));
 
-        this.scene.add(new Interface.Line([[-0.3,-2,0],[-1,-2,0],[-1.3,-2.3,0],[-1.3,-4,0],[-1,-4.3,0],[-1,-5,0]], this.config.cMain,1,0.5));
-        this.scene.add(new Interface.Line([[0.3,-2,0],[1,-2,0],[1.3,-2.3,0],[1.3,-4,0],[1,-4.3,0],[1,-5,0]], this.config.cMain,1,0.5));
+
+        this.scene.add(new Interface.Circle(100,256,[220,0,0],this.config.cMain,4,0.7));
+        this.scene.add(new Interface.Circle(60,256,[220,0,-100],this.config.cMain,2,0.4));
+        this.scene.add(new Interface.Circle(60,256,[220,0,100],this.config.cMain,2,0.4));
+
+
+        this.scene.add(new Interface.Circle(20,256,[0,0,0],this.config.cMain,4,0.7));
+        this.scene.add(new Interface.Circle(384,256,[0,0,0],this.config.cMain,4,0.7));
 
         // We will use 2D canvas element to render our HUD.
         var hudCanvas = document.createElement('canvas');
 
         // Again, set dimensions to fit the screen.
-        hudCanvas.width = 512;
-        hudCanvas.height = 256;
+        hudCanvas.width = 1024;
+        hudCanvas.height = 512;
 
         // Get 2D context and draw something supercool.
         var hudBitmap = hudCanvas.getContext('2d');
-        hudBitmap.font = '1000px "Roboto"';
+        hudBitmap.font = '100px "Roboto"';
         hudBitmap.textAlign = 'center';
         hudBitmap.fillStyle = "rgba(255,255,255,0.9)";
         hudBitmap.fillText('10', 256, 128);
@@ -83,39 +100,70 @@ Interface.Main = {
         hudTexture.needsUpdate = true;
 
         // Create HUD material.
-        var material = new THREE.MeshBasicMaterial( {map: hudTexture} );
+        var material = new THREE.MeshBasicMaterial({map:hudTexture});
         material.transparent = true;
 
         // Create plane to render the HUD. This plane fill the whole screen.
-        var planeGeometry = new THREE.PlaneGeometry(1000,500);
+        var planeGeometry = new THREE.PlaneGeometry(128,64);
         var plane = new THREE.Mesh(planeGeometry,material);
         plane.position.x = 0;
         plane.position.y = 0;
-        this.scene.add(plane);
+        //this.scene.add(plane);
 
         this.render();
 
         this.update();
     },
 
-    createScene : function () {
-        this.scene = new THREE.Scene();
-        this.camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / -2, window.innerHeight / 2, 0.1, 1000);
-        this.camera.position.z = 1000;
-        this.renderer = new THREE.WebGLRenderer({
-            antialias:true,
-            alpha:true
-        });
+    handleWindowResize : function() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
 
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(this.renderer.domElement);
+        this.updateScene();
+    },
 
-        if(this.debug) {
-            var helper = new THREE.GridHelper(10, 20, 0x000000, 0x000000 );
-            helper.position.y = 0;
-            helper.rotateX(90 * Math.PI / 180);
-            this.scene.add(helper);
+    updateScene : function () {
+
+        if(!this.scene) this.scene = new THREE.Scene();
+
+
+        if(!this.camera) {
+            this.cameraObject = new THREE.Object3D();
+            this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 0.1, 10000);
+            this.camera.aspect = this.width / this.height;
+            this.camera.position.z = 1024;
+            this.camera.updateProjectionMatrix();
+            this.cameraObject.add(this.camera);
         }
+
+        this.scene.add(this.cameraObject);
+
+
+
+
+        /*
+        if(!this.camera) {
+            this.camera = new THREE.OrthographicCamera(this.width / -2, this.width / 2, this.height / 2, this.height / -2, -1000, 1000);
+        }
+        this.camera.left = this.width / -2;
+        this.camera.right = this.width / 2;
+        this.camera.top = this.height / 2;
+        this.camera.bottom = this.height / -2;
+        this.camera.position.z = 5;
+        this.camera.updateProjectionMatrix();
+        */
+
+
+
+
+        //this.camera.position.z = -10;
+        //this.camera.rotateY(10 * Math.PI / 180);
+
+        if(!this.renderer) {
+            this.renderer = new THREE.WebGLRenderer({antialias:true,alpha:true});
+            document.body.appendChild(this.renderer.domElement);
+        }
+        this.renderer.setSize(this.width, this.height);
     },
 
     update : function() {
@@ -126,7 +174,7 @@ Interface.Main = {
 
         var delta = this.clock.getDelta();
 
-        this.objects.leftEye.rotateZ(delta * 100 * Math.PI / 180);
+        this.cameraObject.rotateY(delta * -10 * Math.PI / 180);
 
         window.requestAnimationFrame(this.render.bind(this));
 
@@ -150,11 +198,7 @@ Interface.Main = {
     },
 
     handleUpdateFail : function(data) {
-
-        $('#header-nav .navbar-brand span').addClass('flashing');
-
         setTimeout(this.update.bind(this), 200);
-
     },
 
     handleButtonClick : function(e) {
